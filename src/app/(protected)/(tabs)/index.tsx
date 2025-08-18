71ed7de17caee6a13bbb650fee954e7ccd052577
Julio Davila <juliodacer@gmail.com>
2025-08-18 17:52:26 -0500

    feat: fetching posts from supabase

--- src/app/(protected)/(tabs)/index.tsx
+++ src/app/(protected)/(tabs)/index.tsx
@@ -1,8 +1,34 @@
 import { FlatList, View } from "react-native";
 import PostListItem from "../../../components/PostListItem";
-import posts from "../../../../assets/data/posts.json";
+import { useEffect, useState } from "react";
+import { supabase } from "../../../lib/supabase";
+import { Post } from "../../../types";
+
+// type Post = Tables<"posts"> & {
+//   user:
+// }
 
 export default function HomeScreen() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    fetchPost();
+  }, []);
+
+  const fetchPost = async () => {
+    const { data, error } = await supabase.from("posts").select(`
+        *,
+        group:groups(*),
+        user:users!posts_user_id_fkey(*)
+        `);
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("QUERY", JSON.stringify(data, null, 2));
+      setPosts(data);
+    }
+  };
+
   return (
     <View>
       <FlatList
